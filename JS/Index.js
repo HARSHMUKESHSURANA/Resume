ee0dabbe853c0bfe892de1772427821dd52b2d5f
HARSHMUKESHSURANA <149784501+HARSHMUKESHSURANA@users.noreply.github.com>
2023-11-03 15:53:56 +0530

    Added index.html

---
+++ JS/Index.js
@@ -0,0 +1,9 @@
+/*======================== typed js ==================*/
+const typed = new Typed('.multiple-text',{
+    strings:['','Frontend Developer',' Video Editor','Stock Analyzer'],
+    typeSpeed:150,
+    backSpeed:50,
+    backDelay:100,
+    loop:true
+});
+
